09821370bb838846d367d04ade0b2b4508de1705
dinorex01 <danielazoulay2010@gmail.com>
2024-02-09 14:20:48 -0300

    Estilização  e componentização das notas

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import logoNlwExpert from "./assets/logo-nlw-expert.svg";
+import { NewNoteCard } from "./components/New-Note-Card";
+import { NoteCard } from "./components/Note-Card";
+
 export function App() {
-  return (
-      <h1>My App</h1>
-  )
+	return (
+		<div className="mx-auto max-w-6xl my-12 space-y-6">
+			<img src={logoNlwExpert} alt="NLW Expert" className="w-80"/>
+			<form className="w-full my-6">
+				<input
+					type="text"
+					placeholder="Busque em suas notas..."
+					className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+				/>
+			</form>
+			{/* div sem conteudo/vazia  apenas para estilo*/}
+			<div className="h-px bg-slate-700" />
+
+			<div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+				<NewNoteCard />
+				<NoteCard />
+				<NoteCard />
+				<NoteCard />
+			</div>
+		</div>
+	);
 }
---
+++ src/components/Note-Card.tsx
@@ -0,0 +1,14 @@
+export function NoteCard() {
+	return (
+		<button className="text-left rounded-md bg-gradient-to-b from-slate-800/100 to-slate-800/10 p-5 space-y-3 overflow-hidden hover:ring-2 hover:ring-slate-600 outline-none focus-visible:ring-2 focus-visible:ring-lime-200">
+			<span className="text-sm font-medium text-slate-300">Há 2 dias</span>
+			<p className="text-sm leading-6 text-slate-400">
+				Grave uma nota em áudio que será convertida para texto automaticamente. Grave uma nota em áudio que será
+				convertida para texto automaticamente. Grave uma nota em áudio que será convertida para texto
+				automaticamente. Grave uma nota em áudio que será convertida para texto automaticamente. Grave uma nota
+				em áudio que será convertida para texto automaticamente. Grave uma nota em áudio que será convertida
+				para texto automaticamente.
+			</p>
+		</button>
+	);
+}
